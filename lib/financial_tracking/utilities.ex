69d195e30a0eb83732bbc0a0c124b3d153707067
Faisal <felkhouri@ucsd.edu>
2026-06-26 16:07:03 -0700

    minor utilities fix

--- lib/financial_tracking/utilities.ex
+++ lib/financial_tracking/utilities.ex
@@ -1,16 +1,12 @@
 defmodule FinancialTracking.Utilites do
-  @moduledoc """
-  Miscellaneous functions
-  """
-  @app :financial_tracking
 
   @doc """
   Returns all the children of an office recursively
   """
   def get_office_with_children(id) do
-    case Repo.get(Office, id) do
+    case FinancialTracking.Repo.get(Office, id) do
       nil -> {:error, :not_found}
-      office -> {:ok, Repo.preload(office, :children)}
+      office -> {:ok, FinancialTracking.Repo.preload(office, :children)}
     end
   end
 
